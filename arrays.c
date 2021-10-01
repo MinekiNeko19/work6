acaa941c68871e88dc2aad91ff8cd1ae13e0ca6a
Sarah Leong <sleong20@stuy.edu>
2021-10-01 16:10:03 -0400

    adds random numbers into the array

--- arrays.c
+++ arrays.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <time.h>
 
 int main() {
     int arr[10];
     arr[0] = 0;
     int i;
+    srand(time(NULL));
 
     for (i = 1; i < 10; i++) {
-        
+        int x = rand();
+        arr[i] = x;
+    }
+
+    for (i = 0; i < 10; i++) {
+        printf("arr[%d]: %d\n",i,arr[i]);
     }
 }
