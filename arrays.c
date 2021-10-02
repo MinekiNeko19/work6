ee6697a511a703b80d7ac4d254a5eb18bb70b957
Sarah Leong <sleong20@stuy.edu>
2021-10-01 20:19:06 -0400

    added values into arr2 with reverse of arr using * pointer method

--- arrays.c
+++ arrays.c
@@ -20,12 +20,13 @@ int main() {
     int *p1 = arr;
     int *p2 = arr2;
 
-    // for (i = 1; i < 10; i++) {
-    //     *p2 = arr[10-i];
-    //     p2++;
-    // }
+    for (i = 0; i < 10; i++) {
+        *p2 = arr[9-i];
+        p2++;
+    }
 
-    // for (i = 0; i < 10; i++) {
-    //     printf("arr2[%d]: %d\n",i,arr[i]);
-    // }
+    for (i = 0; i < 10; i++) {
+        printf("arr2[%d]: %d\n",i,arr2[i]);
+        // printf("arr[%d]: %d\tarr2[%d]: %d\n",i,arr[i],i,arr2[i]);
+    }
 }
